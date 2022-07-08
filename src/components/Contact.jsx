fec455dd8fc2e300b2195f403c325d433ff3dacd
Damon Greenhalgh <damonligreenhalgh@gmail.com>
2022-07-09 01:00:11 +1200

    Fix contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,17 @@
-import { useAnimationDelay } from '../hooks/useAnimationDelay';
 import '../styles/Contact.css';
+import { useAnimationDelay } from '../hooks/useAnimationDelay';
 import Divider from './Divider';
 
 const Contact = (props) => {
     const { sectionIndex, sectionRef } = props;
     useAnimationDelay(sectionRef, true, 1, 0, 4, 1, sectionIndex === 4);
     return (
-        <div className="contact" action="https://public.herotofu.com/v1/5adb50a0-91ea-11ec-8462-6960be7ce578" method="post" id="contact" ref={sectionRef}>
-            <form className="col gap-lg text-center">
+        <div className="contact" id="contact" ref={sectionRef}>
+            <form 
+                className="contact__form" 
+                action="https://public.herotofu.com/v1/5adb50a0-91ea-11ec-8462-6960be7ce578" 
+                method="post"
+            >
                 <h2>Let's Talk!</h2>
                 <p>
                     Got an <b>interesting</b> idea? <b>Feedback</b> on this 
@@ -15,11 +19,38 @@ const Contact = (props) => {
                 </p>
                 <Divider />
                 <div className="contact__row">
-                    <input className="contact__input" name="Name" id="name" type="text" placeholder="Name" required />
-                    <input className="contact__input" name="Email" id="email" type="email" placeholder="Email" required  />
+                    <input 
+                        className="contact__input" 
+                        name="Name" 
+                        id="name" 
+                        type="text" 
+                        placeholder="Name" 
+                        required 
+                    />
+                    <input 
+                        className="contact__input"
+                        name="Email"
+                        id="email"
+                        type="email"
+                        placeholder="Email"
+                        required 
+                    />
                 </div>
-                <textarea cols="30" rows="5" className="contact__input contact__message" name="Message" id="message" type="text" placeholder="Message" required/>
-                <input className="contact__button interactable" type="submit" value="Send" />
+                <textarea 
+                    cols="30"
+                    rows="5"
+                    className="contact__input contact__message"
+                    name="Message"
+                    id="message"
+                    type="text"
+                    placeholder="Message"
+                    required
+                />
+                <input 
+                    className="contact__button interactable"
+                    type="submit"
+                    value="Send"
+                />
             </form>
         </div>
     );
